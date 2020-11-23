2bdb119b41a0709aca82fd3618a6b400b37d319a
loxygenK <loxygen.k@gmail.com>
2020-11-23 12:46:35 +0900

    †追加† サンプルデータを送信するようにした

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,31 @@
+import types.LTInfo
+import types.PresentationInfo
+import types.Presenter
 import ws.CommandParser
-import ws.CommandResult
-import ws.command
-import java.net.Socket
 
 fun main() {
-
-    val echoCommand = command("echo") { args, ws ->
-        println("Received $args")
-        ws?.send("Echoing ${args.joinToString(separator = " ")}")
-        CommandResult.SUCCESS
-    }
-
+    val ltInfoStore = LTInfoStore()
     val commandParser = CommandParser()
-    commandParser.registerCommand(echoCommand)
+    commandParser.registerCommand(ltInfoStore.command)
+
+    ltInfoStore.store(
+        LTInfo(
+            "限界LT #2",
+            "準備中です",
+            PresentationInfo(
+                Presenter(
+                    "loxygen_k",
+                    "フライさん"
+                ),
+                "ZshとFish、どちらを使うべきか",
+                "無難で、ユーザ数も多いZshと、\n" +
+                    "カスタマイズしやすく補完も強いがPOSIX非互換のFish、\n" +
+                    "どちらも一長一短である。\n" +
+                    "我々はどちらを選ぶべきだろうか？",
+                arrayOf("環境構築", "Linux", "シェル"),
+            )
+        )
+    )
 
     LTTimeViewerServer(12333, commandParser).start()
 }
